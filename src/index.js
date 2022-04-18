f9ad739964c051069d0fc9703bae0224a696cd0d
Lidianne Barbosa <lidiannerb@gmail.com>
2022-04-17 23:52:38 -0300

    projeto pronto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
-console.log(validator);
+const form = document.querySelector('#form');
+const input = document.querySelector('#cardNumber')
+const dataValid = document.querySelector('[data-valid]');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const value = input.value;
+    const isValid = validator.isValid(value);
+    dataValid.dataset.valid = isValid;
+});
